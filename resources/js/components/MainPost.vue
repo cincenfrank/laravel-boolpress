715758b02a4046f0c2bfd1d4ca2a62421320c4f5
cincenfrank <fpetrangeli1@gmail.com>
2022-01-18 00:43:23 +0100

    added props

--- resources/js/components/MainPost.vue
+++ resources/js/components/MainPost.vue
@@ -3,7 +3,7 @@
     <div class="row g-0">
       <div class="col-md-5">
         <img
-          src="https://www.lamialiguria.it/images/2020/12/22/manarola_ag.jpg"
+          :src="postData['imageSrc']"
           class="img-fluid rounded w-100"
           alt="..."
           style="max-height: 300px; border-radius: 20px !important"
@@ -12,17 +12,14 @@
       <div class="col-md-7 align-self-center">
         <div class="">
           <p class="text-left">
-            <small class="text-muted">Last update: 15/01/2022 16:35:45</small>
+            <small class="text-muted"
+              >Last update: {{ postData["updatedAt"] }}</small
+            >
           </p>
           <h2 class="text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {{ postData["title"] }}
           </h2>
-          <p class="text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-            magni dolorem sit temporibus enim esse explicabo excepturi quam
-            nostrum eveniet vitae, dolore dolorum corrupti saepe modi laborum
-            delectus autem sint...
-          </p>
+          <p class="text-left">{{ getShortDescription }}</p>
           <a class="btn btn-outline-primary text-left" href="#" role="button"
             >Read More</a
           >
@@ -35,6 +32,18 @@
 <script>
 export default {
   name: "MainPost",
+  props: {
+    postData: Object,
+  },
+  computed: {
+    getShortDescription() {
+      if (this.postData["description"].length > 300) {
+        return this.postData["description"].substring(0, 300) + "...";
+      } else {
+        return this.postData["description"];
+      }
+    },
+  },
 };
 </script>
 
--- resources/js/components/SuggestedPost.vue
+++ resources/js/components/SuggestedPost.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="">
     <img
-      src="https://www.lamialiguria.it/images/2020/12/22/manarola_ag.jpg"
+      :src="postData['imageSrc']"
       class="img-fluid rounded w-100"
       alt="..."
       style="max-height: 200px; border-radius: 10px !important"
     />
     <p class="text-left">
-      <small class="text-muted">Last update: 15/01/2022 16:35:45</small>
+      <small class="text-muted">Last update: {{ postData["updatedAt"] }}</small>
     </p>
     <h4 class="text-left">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      {{ postData["title"] }}
     </h4>
     <a class="btn btn-outline-primary text-left" href="#" role="button"
       >Read More</a
@@ -21,6 +21,7 @@
 <script>
 export default {
   name: "SuggestedPost",
+  props: { postData: Object },
 };
 </script>
 
