498c7577494320d7b4a5263a0c2182c6b5526909
cincenfrank <fpetrangeli1@gmail.com>
2022-01-17 16:46:08 +0100

    added vue default guest route and navbar component

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-100">
+    <GuestNavigationBar></GuestNavigationBar>
+    <div class="container text-center">
+      <h1 class="text-success">Vue Page</h1>
+      <h4>Work in Progress...</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import GuestNavigationBar from "../components/GuestNavigationBar.vue";
+export default {
+  name: "App",
+  components: { GuestNavigationBar },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
